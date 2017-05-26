4e3d5d8131390b7d5765befe4fe1536b2380d6f9
524855 <annguyen@C02S7148FVH8.home>
2017-05-26 17:39:58 -0400

    validate create/login user completed

--- project5/gop_app_frontend/public/app.js
+++ project5/gop_app_frontend/public/app.js
@@ -1,13 +1,14 @@
 
 var TEST = ''
+console.clear();
 const app_domain = 'http://localhost:8000/'
 const api_domain = 'http://localhost:3000/'
 const app = angular.module('GopApp', []);
 
 app.controller('UserController', ['$http', function($http){
 
-  this.welcomeMsg = "Welcome to Gospatial online app"
-  this.projects = "";
+  //this.welcomeMsg = "Welcome to Gospatial online app"
+  //this.projects = "";
 
 
  TEST = this.welcomeMsg ;
@@ -22,9 +23,11 @@ this.getFeatureUrl = function (){
   }.bind(this));
 };
 
-//******************** create User**************
+//******************** Create User**************
+
 this.createUser = function(){
-  this.username = "vv";
+  console.log('Creating new User ');
+  this.username = "szzxz";
   this.password = "pass123";
   this.email    = "vvemail@gmail.com";
   this.name     = "Full Name";
@@ -37,14 +40,37 @@ this.createUser = function(){
       email   : this.email,
       password: this.password
     }
-  }).then(function(response){
-      console.log(response);
-  }.bind(this));
-};
+  }).then(response => {
+    console.log(response);
+      if(response.data.message){
+        this.createUserMsg = response.data.message //msg set from API
+        console.log(this.createUserMsg);
+      }
+      else {
+        this.createUserSuccessMsg = "Create User Success"
+        console.log(this.createUserSuccessMsg);
+      }
+    })
+
+};// end of createUser
 //******************** User Login**************
+// this.login = function(){
+//   this.username = "a";
+//   this.password = "pass123";
+//   $http({
+//     method: "POST",
+//     url   : api_domain + "users/login",
+//     data:{
+//       username: this.username,
+//       password: this.password
+//     }
+//   }).then(function(response){
+//       console.log(response);
+//   }.bind(this));
+// };
 this.login = function(){
-  this.username = "vv";
-  this.password = "pass123";
+    this.username = "xx";
+    this.password = "pass123";
   $http({
     method: "POST",
     url   : api_domain + "users/login",
@@ -52,16 +78,21 @@ this.login = function(){
       username: this.username,
       password: this.password
     }
-  }).then(function(response){
+  }).then(response =>{
       console.log(response);
-  }.bind(this));
+      if(response.data.status == 200){
+        this.sucessfulLoginMsg = response.data.message
+      }else {
+        this.failedLoginMsg = response.data.message
+      }
+  })
 };
-
 //************ Testing section **********
 // createUser();    //worked
-this.getFeatureUrl(); //worked
+//this.getFeatureUrl(); //worked
+//this.createUser();
 
-// this.login();
+this.login();
 
 
 
